b7286fb0f8de9e2ce7a42e609e7bd8b306bada78
Oleksandr Shturmov <oleks@oleks.info>
2016-04-14 16:00:55 +0200

    exit if rounding mode cannot be set

--- src/creeping-crud/sum.c
+++ src/creeping-crud/sum.c
@@ -1,16 +1,22 @@
-#include <stdio.h>
-#include <fenv.h>
+#include <fenv.h> // fesetround, FE_*
+#include <stdio.h> // fprintf, stderr
+#include <stdlib.h> // EXIT_*
 
 int main() {
   size_t i;
 
-  fesetround(ROUND);
+  if (fesetround(ROUND) != 0) {
+    fprintf(stderr, "Couldn't mandate rounding mode.\n");
+    exit(EXIT_FAILURE);
+  }
 
   REAL sum = START_WITH;
   for (i = 0; i < N_ADDITIONS; ++i) {
     sum += ADDEND;
   }
   printf("%f\n", sum);
+
+  exit(EXIT_SUCCESS);
 }
 
 // Check that IEEE is actually in use on the target.
