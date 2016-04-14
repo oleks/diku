47efd67b0dddc02461f6e1e51de75227304c241d
Oleksandr Shturmov <oleks@oleks.info>
2016-04-14 15:52:56 +0200

    check that target actually IS IEEE-compliant

--- src/creeping-crud/sum.c
+++ src/creeping-crud/sum.c
@@ -12,3 +12,9 @@ int main() {
   }
   printf("%f\n", sum);
 }
+
+// Check that IEEE is actually in use on the target.
+// http://stackoverflow.com/a/753018/5801152
+#ifndef __STDC_IEC_559__
+#error "Requires IEEE 754 floating point!"
+#endif
