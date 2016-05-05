cee6f47019751971addc5ee86d5007b2e6b1e93e
Oleksandr Shturmov <oleks@oleks.info>
2016-05-05 15:17:32 +0200

    Index the other fields as well

--- exp/unums/unum.cpp
+++ exp/unums/unum.cpp
@@ -17,7 +17,7 @@ Unum::isNaN()
 {
   // In the absence of a nan summary bit..
   // A unum is NaN if all bits after the sign bit are 1.
-  unsigned m = maskAfter<this->sign_bit>(this->bf);
+  unsigned m = maskAfter<this->sign>(this->bf);
   return (m & this->bf.store) == (m & this->bf.typeMask);
 }
 
@@ -26,7 +26,7 @@ Unum::isZero()
 {
   // In the absence of a zero summary bit..
   // A unum is zero if all bits after the sign bit are 0.
-  unsigned m = maskAfter<this->sign_bit>(this->bf);
+  unsigned m = maskAfter<this->sign>(this->bf);
   return (m & this->bf.store) == 0;
 }
 
@@ -35,5 +35,5 @@ Unum::isNeg()
 {
   // In the absence of a neg summary bit..
   // A unum is negative if the sign bit is set, and it is not zero.
-  return get<this->sign_bit>(this->bf) && !this->isZero();
+  return get<this->sign>(this->bf) && !this->isZero();
 }
