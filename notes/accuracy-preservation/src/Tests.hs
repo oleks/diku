c9e7162add234a39c39c97528b81e6d2c78bf509
oleks <oleks@oleks.info>
2016-10-04 14:14:22 +0200

    Those were not associativity tests

--- notes/accuracy-preservation/src/Tests.hs
+++ notes/accuracy-preservation/src/Tests.hs
@@ -7,15 +7,9 @@ import Test.Tasty.QuickCheck
 
 properties :: TestTree
 properties = testGroup "QuickCheck"
-  [ testProperty "expsum is associative" $
-    \ds -> foldl' (\es d -> expsum es [d]) ds ==
-      foldl' (\es d -> expsum [d] es) ds
-  , testProperty "esum doesn't grow unboudedly" $
+  [ testProperty "esum doesn't grow unboudedly" $
     \ds -> length (esum ds) < 41
   ]
-  where
-      foldl' :: ([Double] -> Double -> [Double]) -> [Double] -> [Double]
-      foldl' f = nozero . foldl f []
 
 tests :: TestTree
 tests = testGroup "Tests" [properties]
