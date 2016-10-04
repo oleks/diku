44b0a3d9a9cb5d3c023ce35ab2a0252f64a9f110
oleks <oleks@oleks.info>
2016-10-04 11:59:08 +0200

    expsum is associative

--- notes/accuracy-preservation/src/Eft.hs
+++ notes/accuracy-preservation/src/Eft.hs
@@ -1,11 +1,17 @@
 module Eft where
 
+import Data.Char ( intToDigit )
+import Data.Word ( Word64 )
+import Data.List ( intercalate )
+import Numeric ( showHex, showIntAtBase )
+import Unsafe.Coerce ( unsafeCoerce )
+
 newtype ECDouble = ECDouble (Double, [Double])
   deriving (Show)
 
 -- Knuth 1997
-sum2 :: Double -> Double -> (Double, Double)
-sum2 a b =
+twosum :: Double -> Double -> (Double, Double)
+twosum a b =
   let x     = (+) a   b
       b'    = (-) x   a
       a'    = (-) x   b'
@@ -14,15 +20,51 @@ sum2 a b =
       y     = (+) a'' b''
   in  (x, y)
 
-nsum :: [Double] -> ECDouble
-nsum xs = foldl sum' (ECDouble (0.0, [])) xs
-  where
-    sum' (ECDouble (r1, es)) x =
-      let (r2, e2) = sum2 r1 x
-      in  ECDouble (r2, e2:es)
+-- Shewchuk 1997
+growexp :: [Double] -> Double -> [Double]
+growexp es b =
+  let
+    ps = scanl (\ (_, q) e -> twosum q e) (0.0, b) es
+    vs = tail $ map fst ps
+    vn = snd $ last ps
+  in vs ++ [vn]
 
-opt :: ECDouble -> ECDouble
-opt (ECDouble (x, es)) =
-  let (ECDouble (e', es')) = nsum es
-      es'' = filter (/= 0.0) $ es'
-  in  ECDouble (x, e':es'')
+-- Shewchuk 1997
+expsum :: [Double] -> [Double] -> [Double]
+expsum es [0.0] = es  -- fast
+expsum [0.0] fs = fs  -- fast
+expsum es fs =
+  let (xs, ys) = foldl expsum' ([], es) fs
+  in reverse xs ++ ys
+  where
+    expsum' (hs', hs) f =
+      let (h:hs'') = growexp hs f
+      in  if h == 0.0     -- fast
+          then (hs', hs'')
+          else (h:hs', hs'')
+
+nozero :: [Double] -> [Double]
+nozero = filter (/= 0.0)
+
+esum :: [Double] -> [Double]
+esum = nozero . foldl growexp []
+
+-- Useful for debugging
+
+hex :: Double -> String
+hex d = "0x" ++ (showHex ((unsafeCoerce d)::Word64) "")
+
+bin :: Double -> String
+bin d =
+  let
+    raw = showIntAtBase 2 intToDigit ((unsafeCoerce d)::Word64) ""
+    pad = take (64 - length raw) $ repeat '0'
+    bits = pad ++ raw
+    s = take 1 bits
+    e = take 11 $ drop 1 bits
+    f = drop 12 bits
+  in
+    intercalate " " [s, e, f]
+
+eshow :: (Double -> String) -> [Double] -> IO ()
+eshow f = (mapM_ putStrLn) . (map f) . nozero
---
+++ notes/accuracy-preservation/src/Tests.hs
@@ -0,0 +1,25 @@
+module Tests where
+
+import Eft
+
+import Test.Tasty
+import Test.Tasty.QuickCheck
+
+properties :: TestTree
+properties = testGroup "QuickCheck"
+  [ testProperty "" $
+    \ds -> foldl' growexp ds ==
+      foldl' (\es d -> expsum es [d]) ds
+  , testProperty "expsum is associative" $
+    \ds -> foldl' (\es d -> expsum es [d]) ds ==
+      foldl' (\es d -> expsum [d] es) ds
+  ]
+  where
+      foldl' :: ([Double] -> Double -> [Double]) -> [Double] -> [Double]
+      foldl' f = nozero . foldl f []
+
+tests :: TestTree
+tests = testGroup "Tests" [properties]
+
+main :: IO ()
+main = defaultMain tests
