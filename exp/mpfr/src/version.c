bad986f25cbb1a1e825a9ed8566b04bd9a5506de
oleks <oleks@oleks.info>
2017-01-25 21:32:30 +0100

    An MPFR version inspector

---
+++ exp/mpfr/src/version.c
@@ -0,0 +1,14 @@
+/// MPFR Version Insepctor
+///
+/// Shamelessly copied from the MPFR documentation:
+/// http://www.mpfr.org/mpfr-current/mpfr.html#Basic-Arithmetic-Functions
+
+#include <stdio.h>
+#include <mpfr.h>
+int main (void)
+{
+  printf ("MPFR library: %-12s\nMPFR header:  %s (based on %d.%d.%d)\n",
+          mpfr_get_version (), MPFR_VERSION_STRING, MPFR_VERSION_MAJOR,
+          MPFR_VERSION_MINOR, MPFR_VERSION_PATCHLEVEL);
+  return 0;
+}
